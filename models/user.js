16c056e68db4f7ab60fd95e5c93c8146fe38aaf0
Adebobola01 <m.oyedunmade@gmail.com>
2022-09-27 02:18:42 +0100

    signup controller and route

--- models/user.js
+++ models/user.js
@@ -15,7 +15,7 @@ const userSchema = new Schema({
     },
     status: {
         type: String,
-        require: true,
+        default: "I am new!",
     },
     posts: [
         {
--- routes/auth.js
+++ routes/auth.js
@@ -1,24 +1,28 @@
 const express = require("express");
 const router = express.Router();
 const { check, body } = require("express-validator");
-const feedController = require("../controller/feed");
+const authController = require("../controller/auth");
 const User = require("../models/user");
 
-router.put("/signup", [
-    body("email")
-        .trim()
-        .isEmail()
-        .withMessage("Please enter a valid email!")
-        .normalizeEmail()
-        .custom((value, { req }) => {
-            return User.findOne({ email: value }).then((userDoc) => {
-                if (userDoc) {
-                    return Promise.reject("Email address already exists!");
-                }
-            });
-        }),
-    body("password").trim().isLength({ min: 5 }),
-    body("name").trim().not().isEmpty(),
-]);
+router.put(
+    "/signup",
+    [
+        body("email")
+            .trim()
+            .isEmail()
+            .withMessage("Please enter a valid email!")
+            .normalizeEmail()
+            .custom((value, { req }) => {
+                return User.findOne({ email: value }).then((userDoc) => {
+                    if (userDoc) {
+                        return Promise.reject("Email address already exists!");
+                    }
+                });
+            }),
+        body("password").trim().isLength({ min: 5 }),
+        body("name").trim().not().isEmpty(),
+    ],
+    authController.signup
+);
 
 module.exports = router;
